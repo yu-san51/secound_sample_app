29148a0bf355b808fbfdbf6415a51e53cff08d73
yu-san51 <nekonyan.yuki51@gmail.com>
2020-03-19 10:08:39 +0000

    [Finish] 4-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '232e2bb22d8e6a10e132d2d5191f6522e7354152f7dc7748dce1cd4aec3c70396c164a5f637411397433a0f7cc63849d00e2b13d0c177c40d1d5da86ed89b4ca'
